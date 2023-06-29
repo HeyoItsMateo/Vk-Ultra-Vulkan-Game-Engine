6ce66ec98c796a419033c81b8bd833a01d021e22
matts <matts@MATTILION>
2023-06-29 13:28:04 -0700

    Test

--- shaders/compile.bat
+++ shaders/compile.bat
@@ -1,4 +1,5 @@
 C:/VulkanSDK/1.3.243.0/Bin/glslc.exe shader.vert -o vert.spv
+C:/VulkanSDK/1.3.243.0/Bin/glslc.exe test.vert -o testVert.spv
 C:/VulkanSDK/1.3.243.0/Bin/glslc.exe shader.frag -o frag.spv
 C:/VulkanSDK/1.3.243.0/Bin/glslc.exe shader.comp -o comp.spv
 pause
